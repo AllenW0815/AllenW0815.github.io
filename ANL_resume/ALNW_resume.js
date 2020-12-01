a1a5f47ec1c45e69d549389a93ca47924a3db2de
Allen <ro81516@gmail.com>
2020-12-02 00:06:37 +0800

    加上滑鼠小圈圈

--- ANL_resume/ALNW_resume.js
+++ ANL_resume/ALNW_resume.js
@@ -7,6 +7,8 @@ const title = document.querySelector('.title')
 const h3 = document.querySelectorAll('h3')
 const link = document.querySelector('.link')
 const hire = document.querySelector('.hire')
+    //滑鼠圈圈用
+const pointer = document.querySelector('#pointer')
 
 //加掛事件監聽
 container.addEventListener('mousemove',(e)=>{
@@ -26,7 +28,7 @@ container.addEventListener('mouseenter',(e)=>{
     avatar.style.transform = `translateZ(120px)`
     title.style.transform = `translateZ(80px)`
     h3.forEach((e)=>{
-        e.style.transform = `translateZ(40px)`
+        e.style.transform = `translateZ(80px)`
     })
     link.style.transform = `translateZ(80px)`
     hire.style.transform = `translateZ(120px)`
@@ -35,13 +37,16 @@ container.addEventListener('mouseenter',(e)=>{
 //動畫取消
 container.addEventListener('mouseleave',(e)=>{
     //滑鼠離開container的時候加上transition，解決動畫斷層
-    card.style.transition='0.5s ease'
+    card.style.transition='0.8s ease'
     card.style.transform = null
     // card.style.transform = 'none'
     // card.style.transform = `rotateX(0deg) rotateY(0deg)`
     //DOM彈出效果取消
     avatar.style.transform = `translateZ(0px)`
     title.style.transform = `translateZ(0px)`
+    h3.forEach((e)=>{
+        e.style.transform = `translateZ(0px)`
+    })
     link.style.transform = `translateZ(0px)`
     hire.style.transform = `translateZ(0px)`
 })
@@ -54,4 +59,13 @@ avatar.addEventListener('click',(e)=>{
 //上班了
 hire.addEventListener('click',(e)=>{
     hire.innerHTML = `<button>請撥 0933 909 238</button>`
+})
+
+//滑鼠圈圈
+window.addEventListener('mousemove',(e)=>{
+    console.log('e.pageX='+e.pageX,'e.pageY='+e.pageY);
+    //圈圈尺寸20*20
+    let circleX=e.pageX-10
+    let circleY=e.pageY-10
+    pointer.style.transform = `translate(${circleX}px,${circleY}px)`
 })
