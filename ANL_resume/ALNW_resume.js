eb5bcad7f53790b10a695b968eef803e7ba316dc
Allen <ro81516@gmail.com>
2020-12-08 17:59:08 +0800

    修改

--- ANL_resume/ALNW_resume.js
+++ ANL_resume/ALNW_resume.js
@@ -7,10 +7,20 @@ const title = document.querySelector('.title')
 const h3 = document.querySelectorAll('h3')
 const link = document.querySelector('.link')
 const hire = document.querySelector('.hire')
+const hireBtn = document.querySelector('#hireBtn')
     //滑鼠圈圈用
 const pointer = document.querySelector('#pointer')
     //水波紋用
 const body = document.querySelector('body')
+    //工作經歷用
+const workExpBtn = document.querySelector('#workExpBtn')
+const workExp = document.querySelector('.workExp')
+    //自我介紹用
+const aboutBtn = document.querySelector('#aboutBtn')
+const about = document.querySelector('.about')
+    //聯絡方式
+const contact = document.querySelector('#contact')
+const contactBtn = document.querySelector('#contactBtn')
 
 //加掛事件監聽
 container.addEventListener('mousemove',(e)=>{
@@ -58,9 +68,22 @@ avatar.addEventListener('click',(e)=>{
     console.log('hi');
 })
 
-//上班了
+//面試了
 hire.addEventListener('click',(e)=>{
-    hire.innerHTML = `<button>請撥 0933 909 238</button>`
+    if(hireBtn.innerHTML=='面試邀約'){
+        hireBtn.innerHTML='請撥0933-909-238'
+    }else{
+        hireBtn.innerHTML='面試邀約'
+    }
+})
+
+//聯絡方式
+contactBtn.addEventListener('click',(e)=>{
+    if(contact.innerHTML =='我叫王淇生，目前正在學習網頁設計及前端的相關技術，期望成為一位獨當一面的工程師、設計師'){
+        contact.innerHTML =`<i class="fas fa-envelope"></i>&nbsp;:&nbsp;alnwangchi@gmail.com<br/><i class="fas fa-mobile-alt"></i>&nbsp;:&nbsp;0933-909-238`
+    } else{
+        contact.innerHTML ='我叫王淇生，目前正在學習網頁設計及前端的相關技術，期望成為一位獨當一面的工程師、設計師'
+    }
 })
 
 //滑鼠圈圈
@@ -85,4 +108,14 @@ window.addEventListener('click',(e)=>{
     setTimeout(()=>{
         ripples.remove();
     },1200)
+})
+
+//工作經驗
+workExpBtn.addEventListener('click',(e)=>{
+    workExp.classList.toggle('workExp1')
+})
+
+//自我簡介
+aboutBtn.addEventListener('click',(e)=>{
+    about.classList.toggle('about1')
 })
